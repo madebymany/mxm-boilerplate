422279950b01dc58d542e70b333b45dfca06f70b
Andrew Walker <andy@madebymany.co.uk>
2013-04-08 18:54:37 +0100

    create application directory

--- lib/generators/mxm/layout/layout_generator.rb
+++ lib/generators/mxm/layout/layout_generator.rb
@@ -2,10 +2,11 @@ module Mxm
   module Generators
     class LayoutGenerator < ::Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
-     
+
       def generate_layout
         remove_file "app/views/layouts/application.html.erb"
         directory "layouts", "app/views/layouts"
+        directory "application", "app/views/application"
       end
     end
   end
