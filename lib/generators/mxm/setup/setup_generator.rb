35fe85eee8e8ff0bc9c4d7b0a15c216590c8da9d
Andrew Walker <andy@madebymany.co.uk>
2013-04-08 18:55:18 +0100

    run frontend and ammend the development rb for sass source maps compatibility

--- lib/generators/mxm/setup/setup_generator.rb
+++ lib/generators/mxm/setup/setup_generator.rb
@@ -2,10 +2,20 @@ module Mxm
   module Generators
     class SetupGenerator < ::Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
-     
+
       def generate_all
         invoke "mxm:assets", ["application"]
         invoke "mxm:layout", ["application"]
+        invoke "mxm:frontend", ["application"]
+
+        insert_into_file "config/environments/development.rb", :after => "Application.configure do\n" do
+          [
+            "config.assets.debug = true",
+            "config.sass.debug_info = true",
+            "config.sass.line_comments = false # source maps don't get output if this is true"
+          ].join("\n")
+        end
+
       end
     end
   end
